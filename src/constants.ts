231a733b0c79694102b90798706509bc942b8db8
yuto343 <namawasabi1229@gmail.com>
2024-12-11 19:00:30 +0900

    view transitionを実装

---
+++ src/constants.ts
@@ -0,0 +1,32 @@
+export const boards = [
+  {
+    brand: "CAPITA",
+    img: "http://www.capitasnowboarding.jp/img/2023boards/board/doa_148.png",
+    name: "DOA",
+    desc: "アイスバーンからパウダーまであらゆるコンディションに対応できる独自のハイブリッド形状を採用。コアは軽量でしなやか、驚く程簡単に操作出来る。それでいて反発、安定性を生む素材が絶妙にブレンドされることで、一切隙のないボードが完成した。",
+  },
+  {
+    brand: "RIDE",
+    img: "https://makeshop-multi-images.akamaized.net/k2japan/itemimages/000000001151_sD6bkKg.jpg",
+    name: "WARPIG",
+    desc: "RIDEが誇るRoll-in-Construction技術“スリムウォール”を採用し、軽量化と高い振動吸収性を実現しました。サイドウォールが3D形状にデザインされ、エッジへのパワー伝達を向上させています。独特なWARPIGのシェイプは近年のスノーボードでは類を見ない多様性を持ち合わせていて、オールマウンテン、フリースタイル、どちらのカテゴリーでも抜群のパフォーマンスを発揮します。カービングからビックジャンプ、バックカントリーのビックラインまで滑る場所を選びません。テーパードされたBi-Radial-Sidecutはエッジグリップが良く、一般的な細い板並みに早いエッジ切り換えが可能です。一方、ワイド形状なのでパウダーでも安定性と浮力を足元に感じられます。WARPIGは誰もが認めるオールマウンテンボードです",
+  },
+  {
+    brand: "CAPITA",
+    name: "SPRING BREAK SLUSH SLASHERS",
+    img: "http://www.capitasnowboarding.jp/img/2023boards/board/slush_slashers_139.png",
+    desc: "クラシックなサーフロッカーシェイプ。ワイドなウェストが安定感を出し、柔らかいトーションで自在に操作ができる。パウダーから春のクラッシュスノーまで幅広く対応できるショートファンボード。短いテールでどこでもスラッシュしよう！",
+  },
+  {
+    brand: "ROME",
+    name: "Ravine",
+    img: "https://www.hasco.co.jp/romesds/wp-content/uploads/sites/8/2024/07/2425Rome_Board_Ravine.jpg",
+    desc: "最大限の浮遊感と素早いターンのためのノーズのディレクショナル・ダイアモンド3Dとテーパー。",
+  },
+  {
+    brand: "Bataleon",
+    name: "Whatever",
+    img: "https://bataleon.com/cdn/shop/files/Bataleon-Whatever_2025-Bataleon-Snowboards_-product-image-2025-Whatever_2025-Bataleon-Snowboards_-1_ff8c1857-d571-4913-bdbc-38c397524525.jpg?v=1724326262&width=823",
+    desc: "The Whatever's outline looks strikingly directional, yet its contact surface is twin. It's the ultimate ride for those seeking the best of both worlds. Perfect for park days, hot laps, or powder lines, the Whatever embodies versatility. ",
+  },
+];
